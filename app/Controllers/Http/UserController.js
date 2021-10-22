430ad4043252b4a64e5e58ef18f7fe6c81d977b1
Selvy Fitriani <selvyfiriani@gmail.com>
2021-10-22 10:39:48 +0700

    able to send response for invalid token

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,11 +5,16 @@ const AuthController = use('App/Controllers/Http/AuthController')
 class UserController {
   async getMe({ req, response }) {
     const authController = new AuthController()
-    const data = await authController.validateToken({ req, response })
-    
-    const user = { name: data['name'], email: data['email'] }
 
-    response.status(200).send(user)
+    try {
+      const data = await authController.validateToken({ req, response })
+      const user = { name: data['name'], email: data['email'] }
+  
+      response.status(200).send(user)
+    } catch (err) {
+      response.status(401).send(err)
+    }
+   
   }
 }
 
