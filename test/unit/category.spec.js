7cc9684a02284800f48b6e7b2af3908fa715ee8f
Selvy Fitriani <selvyfiriani@gmail.com>
2021-11-11 11:22:57 +0700

    refactor: change tests name

--- test/unit/category.spec.js
+++ test/unit/category.spec.js
@@ -3,13 +3,13 @@
 const { test } = use('Test/Suite')('Category')
 const Category = use('App/Models/Category')
 
-test('isValid should return true for valid category', async ({ assert }) => {
+test('isValid() should return true for valid category', async ({ assert }) => {
   const category = new Category('name', 'description')
 
   assert.isTrue(category.isValid())
 })
 
-test('isValid should return false for empty name', async ({ assert }) => {
+test('isValid() should return false for empty name', async ({ assert }) => {
   const category = new Category(null, 'description')
 
   assert.isFalse(category.isValid())
