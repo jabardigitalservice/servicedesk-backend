11d94da7dd0181ecce64a844c08f6927e6eb258f
Selvy Fitriani <selvyfiriani@gmail.com>
2021-11-14 10:32:51 +0700

    refactor: delete unused function

--- app/Models/Category.js
+++ app/Models/Category.js
@@ -4,17 +4,7 @@
 const Model = use('Model')
 
 class Category extends Model {
-  constructor (name, description) {
-    super()
-    this.name = name
-    this.description = description
-  }
 
-  isValid () {
-    if (!this.name) return false
-
-    return true
-  }
 }
 
 module.exports = Category
